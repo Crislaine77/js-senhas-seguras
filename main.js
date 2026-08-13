6f118d2f8a78f84633694f67576d3a824f8e7d31
Crislaine77 <crislaine.santos.barbosa@escola.pr.gov.br>
2026-08-12 22:46:51 -0300

    aula 12/08

---
+++ main.js
@@ -0,0 +1,41 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+let tamanhoSenha = 12;
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1 ) {
+        tamanhoSenha = tamanhoSenha - 1;
+    }
+    numeroSenha.textContext = tamanhoSenha;
+    geraSenha();
+}
+
+function aumentaTamanho() {
+    if ( tamanhoSenha < 20) {
+         tamanhoSenha = tamanhoSenha + 1;
+    }      
+    numeroSenha.textContext = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+
+for (1 = 0)
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha() {
+    let senha = '';
+    for(let i=0; i< tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * letrasMaiusculas.lenght;
+        numeroAleatorio = math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
